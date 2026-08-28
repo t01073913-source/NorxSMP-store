0e0be00596be9b3f876ffc4f73b55065f40c39b2
t01073913-source <t01073913@gmail.com>
2026-08-28 22:15:29 +0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,20 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+const TOKEN = process.env.DISCORD_TOKEN;
+
+if (!TOKEN) {
+  console.error("DISCORD_TOKEN belum diisi di Railway!");
+  process.exit(1);
+}
+
+client.once("ready", () => {
+  console.log(Bot is ready: ${client.user.tag});
+});
+
+client.login(TOKEN).catch((error) => {
+  console.error("Gagal login:", error.message);
+});
